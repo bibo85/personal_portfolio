b41aa29ec388f7901148f9c022ceb6536af06bbd
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2022-05-20 01:13:53 +0500

    Registration of the model Project in the admin panel

--- personal_portfolio/portfolio/admin.py
+++ personal_portfolio/portfolio/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from portfolio.models import Project
 
-admin.site.register(Project)
 
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title']
--- personal_portfolio/portfolio/models.py
+++ personal_portfolio/portfolio/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='portfolio/images/')
-    url = models.URLField(blank=True)
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    description = models.CharField(max_length=250, verbose_name='Описание')
+    image = models.ImageField(upload_to='portfolio/images/', verbose_name='Изображение')
+    url = models.URLField(blank=True, verbose_name='Ссылка')
+
+    def __str__(self):
+        return self.title
