ce95ddabce4aa0cab093b178f77d87ad9dd59401
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2022-05-23 00:33:29 +0500

    model Blog, displaying blogs

--- personal_portfolio/blog/models.py
+++ personal_portfolio/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    description = models.CharField(max_length=250, verbose_name='Краткое описание')
+    text = models.TextField(verbose_name='Текст статьи')
+    date = models.DateField(verbose_name='Дата')
+    is_published = models.BooleanField(default=False, verbose_name='Статус публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-date']
--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from blog import models
 
 
 def all_blogs_view(request):
-    return render(request, 'blog/all_blogs.html')
-
+    blogs = models.Blog.objects.filter(is_published=True)[:5]
+    context = {
+        'blogs': blogs,
+    }
+    return render(request, 'blog/all_blogs.html', context)
