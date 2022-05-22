7afa5d6e69a954c299029948cc9f2f6f09ca34a7
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2022-05-23 00:40:15 +0500

    del imports

--- personal_portfolio/personal_portfolio/urls.py
+++ personal_portfolio/personal_portfolio/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-import portfolio
-import blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
